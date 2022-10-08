35c34ff78c01323da06f30191b130bde70324c46
Jordy Coder <j.hamwijk34@gmail.com>
2022-10-08 18:22:05 +0200

    Fix menu

--- src/components/NavComponent.js
+++ src/components/NavComponent.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import './navbar.css';
 import logo from '../assets/logo.png';
-import {RiCloseLine, RiMenu3Line} from 'react-icons/ri';
+import {RiCloseLine,RiMenLine, RiMenu3Line} from 'react-icons/ri';
 
 const Menu = () => (
     <>
@@ -31,39 +31,42 @@ const NavComponent = () =>  {
                 <div className="links-logo">
                     <img src={logo} alt="sss" srcSet=""></img>
                 </div>
-                <Menu/>
-            </div>
-            <div className="nav-button">
-                <button  type="button">Hire me</button>
-            </div>
-            <div className="nav-menu">
-                {toggleMenu ? (
-                    <RiCloseLine
-                        color="#3333"
-                        size={27}
-                        onClick={() => setToggleMenu(false)}
-                    />
-                ) : (
-                    <RiMenu3Line
-                        color="#3333"
-                        size={27}
-                        onClick={() => setToggleMenu(true)}
-                    />
-                )}
-                { toggleMenu &&
-                    (
-                        <div className="nav-menu-wrapper">
-                            <div className="nav-menu-wrapper-links">
-                                <Menu />
-                                <div className="nav-menu-wrapper-button">
-                                    <button type="button">Hire me</button>
+                <div className="links-wrapper">
+                    <Menu/>
+                </div>
+                <div className="nav-button">
+                    <button  type="button">Hire me</button>
+                </div>
+                <div className="nav-menu">
+                    {toggleMenu ? (
+                        <RiCloseLine
+                            color="#3333"
+                            size={27}
+                            onClick={() => setToggleMenu(false)}
+                        />
+                    ) : (
+                        <RiMenu3Line
+                            color="#3333"
+                            size={27}
+                            onClick={() => setToggleMenu(true)}
+                        />
+                    )}
+                    { toggleMenu &&
+                        (
+                            <div className="nav-menu-wrapper">
+                                <div className="nav-menu-wrapper-links">
+                                    <Menu />
+                                    <div className="nav-menu-wrapper-button">
+                                        <button type="button">Hire me</button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    )
-                }
+                        )
+                    }
+                </div>
             </div>
         </div>
+
     );
 }
 export default NavComponent;
